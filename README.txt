5afb726053e14450b2f3d16a27b99496ba3b2412
PierrotAix <pdesmazes@gmail.com>
2018-11-28 17:26:40 +0100

    Creation of a simple c-sharp project to demonstrate foreach loop

---
+++ README.txt
@@ -0,0 +1,6 @@
+C Sharp sample code
+
+This is my first test to create a git repository and to share it over the web with GitHub.
+Please be indulgent...
+
+The code is a c-sharp sample.
---
+++ Using-foreach/Using-foreach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Début du test");
+
+            foreach (Char c in "Hello world !")
+            {
+                Console.WriteLine(c);
+            }
+
+
+            Console.WriteLine("Fin du test, tapez sur une touche pour sortir");
+            Console.ReadKey();
+
+
+        }
+    }
+}
