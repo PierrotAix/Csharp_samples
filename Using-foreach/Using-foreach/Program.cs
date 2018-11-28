04e5006530fe0cf15d5f53b6865b563fdfc8e2c6
PierrotAix <pdesmazes@gmail.com>
2018-11-28 17:49:04 +0100

    Enriched with a file Jours.txt that contained lines

--- Using-foreach/Using-foreach/Program.cs
+++ Using-foreach/Using-foreach/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,17 @@ namespace Using_foreach
         {
             Console.WriteLine("Début du test");
 
-            List<string> maListe = new List<string> { "lundi", "mardi", "mercredi", "jeudi", "vendredi" };
+            List<string> maListe = new List<string>();
+
+            // lecture depuis un fichier texte
+            string line;
+            StreamReader file = new StreamReader("jours.txt", Encoding.ASCII);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                maListe.Add(line);
+            }
+            
 
             foreach (string jour in maListe)
             {
